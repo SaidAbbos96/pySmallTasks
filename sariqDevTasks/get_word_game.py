f55d98417f557e219b88c58f9716f88589f0f277
SaidAbbos96 <abbos.xudoyqulov@mail.ru>
2022-01-28 22:17:56 +0500

    add game of finding word

--- sariqDevTasks/get_word_game.py
+++ sariqDevTasks/get_word_game.py
@@ -6,11 +6,18 @@ def mask_m(mask):
 
 def my_game():
     word, temp = random.choice(words_list).lower(), ""
+    word ="salom"
+    while "-" in word or " " in word:
+        word = random.choice(words_list).lower()
+
     mask = list("*" * len(word))
     print(f"Men {len(word)} xonali so'z o'yladim, krel alifbosida harflar kiriting !\nSo'z: {mask_m(mask)}")
     
     while True:
-        char = input("Harfni kiriting: ").strip()[0]
+        char = input("Harfni kiriting: ")
+        if len(char) > 0:
+            char = char.strip()[0]
+
         if char in temp:
             print("Harflarni qayta kirityapsiz !")
             continue
@@ -25,17 +32,9 @@ def my_game():
         else:
             print("Bu harf mavjud emas, yana urunib ko'ring !")
         if word == ''.join(mask):
-            print(mask_m(mask), "\nSo'z to'liq topildi, o'yin tugadi !")
+            print(mask_m(mask), f"\nTabriklayman, so'z {len(temp)} urunishda to'liq topildi, o'yin tugadi !")
             break
         print(f"{'-'*30}\nSo'z: {mask_m(mask)}")
-
-
-
-
-
-
-
-
 
 
 if __name__ == "__main__":
@@ -43,4 +42,10 @@ if __name__ == "__main__":
     while live:
         my_game()
         print("Yana o'ynaylik, yuq demang ?")
-        live = int(input("1 yoki 0 >>> "))
+        live = input("1 yoki 0 >>> ")
+        if live not in ["1", "0"]:
+            live = False
+        else:
+            live = int(live)
+
+
