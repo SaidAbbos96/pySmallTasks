532dad263e4766c0f26e86e5fc25519c410fb8c9
SaidAbbos96 <abbos.xudoyqulov@mail.ru>
2022-02-02 12:28:41 +0500

    maked game of x or o

--- sariqDevTasks/krestnolgame.py
+++ sariqDevTasks/krestnolgame.py
@@ -1,5 +1,7 @@
-game_board = [" "," "," "," "," "," "," "," "," "]
-combinations = [(0,1,2), (3,4,5), (6,7,8), (0,3,6), (1,4,7), (2,5,8), (0,4,8), (2,4,6)]
+game_board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+combinations = [(0, 1, 2), (3, 4, 5), (6, 7, 8), (0, 3, 6),
+                (1, 4, 7), (2, 5, 8), (0, 4, 8), (2, 4, 6)]
+
 
 def print_state(state):
     for i, c in enumerate(state):
@@ -10,23 +12,27 @@ def print_state(state):
 
 # print_state(game_board)
 
+
 def get_winner(state, combinations):
-    for (x, y,z) in combinations:
+    for (x, y, z) in combinations:
         if state[x] == state[y] and state[y] == state[z] and (state[x] == "x" or state[x] == "o"):
             return state[x]
         else:
             return ''
 
+
 def play_game(game_board):
     gamer = "x"
     while (get_winner(game_board, combinations) == ""):
-        position = int(input(f"Janob {gamer} qayerga belgi qo'yishni istaysiz ?")) - 1
+        position = int(
+            input(f"Janob {gamer} qayerga belgi qo'yishni istaysiz ?")) - 1
         game_board[position] = gamer
         print_state(game_board)
         winner = get_winner(game_board, combinations)
         if winner != "":
             print(f"Janob {gamer} siz yutdiz !")
-        
+
         gamer = "x" if gamer == "o" else "o"
+
 
 play_game(game_board)
