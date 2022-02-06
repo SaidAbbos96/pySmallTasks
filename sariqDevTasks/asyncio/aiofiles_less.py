3cefb031279359d2c369a227e4281a08039f6ad2
SaidAbbos96 <abbos.xudoyqulov@mail.ru>
2022-02-06 23:27:26 +0500

    added simple exsample for asyncio for aiofiles

--- sariqDevTasks/asyncio/aiofiles_less.py
+++ sariqDevTasks/asyncio/aiofiles_less.py
@@ -3,21 +3,24 @@ import aiofiles
 
 
 def read_file():
-    with open("my_big_file.txt", "r") as file:
+    with open(r"my_big_file.txt", "r") as file:
         return file.read()
 
 
 async def async_read_file():
-    async with aiofiles.open("my_big_file.txt", "r") as file:
+    async with aiofiles.open(r"my_big_file.txt", "r") as file:
         return await file.read()
+
 
 async def async_main():
     text = await async_read_file()
     print(f"Count words. in file={len(text.split(' '))}")
 
+
 def main():
     text = read_file()
     print(f"Count words. in file={len(text.split(' '))}")
+
 
 if __name__ == "__main__":
     asyncio.run(async_main())
