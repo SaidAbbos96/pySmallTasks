c051cad7f250a7442fe2bddc7bebb37316136376
SaidAbbos96 <abbos.xudoyqulov@mail.ru>
2022-02-06 23:16:12 +0500

    added codes of aiofiles

---
+++ sariqDevTasks/asyncio/aiofiles_less.py
@@ -0,0 +1,24 @@
+import asyncio
+import aiofiles
+
+
+def read_file():
+    with open("my_big_file.txt", "r") as file:
+        return file.read()
+
+
+async def async_read_file():
+    async with aiofiles.open("my_big_file.txt", "r") as file:
+        return await file.read()
+
+async def async_main():
+    text = await async_read_file()
+    print(f"Count words. in file={len(text.split(' '))}")
+
+def main():
+    text = read_file()
+    print(f"Count words. in file={len(text.split(' '))}")
+
+if __name__ == "__main__":
+    asyncio.run(async_main())
+    main()
